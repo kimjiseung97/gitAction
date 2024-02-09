83c28cb7f148a7d31bdc60c18cfcc2f0d1a55c26
jiseung <dickseung23@naver.com>
2024-02-09 14:59:12 +0900

    [feat] gitAction

--- src/main/java/dev/be/git/controller/testController.java
+++ src/main/java/dev/be/git/controller/testController.java
@@ -1,10 +1,13 @@
 package dev.be.git.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class testController {
+
+    @GetMapping
     public ResponseEntity<Object> testController(){
         return ResponseEntity.ok().build();
     }
