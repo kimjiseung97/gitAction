5b825483fab3f364e23090f412256bf693dd29b8
jiseung <dickseung23@naver.com>
2024-03-03 18:00:51 +0900

    [feat] gitAction 반영 테스트

--- src/main/java/dev/be/git/controller/testController.java
+++ src/main/java/dev/be/git/controller/testController.java
@@ -4,21 +4,34 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class testController {
 
     @GetMapping
-    public ResponseEntity<Object> testController(){
+    public ResponseEntity<Object> testController() {
         return ResponseEntity.ok().body("test");
     }
 
     @GetMapping("/hello")
-    public ResponseEntity<?> helloController(){
+    public ResponseEntity<?> helloController() {
         return ResponseEntity.ok().body("hello 자동배포 테스트입니다");
     }
 
     @GetMapping("/hello2")
-    public ResponseEntity<?> hello2Controller(){
+    public ResponseEntity<?> hello2Controller() {
         return ResponseEntity.ok().body("hello2 자동반영되나요?.");
+
+    }
+
+    @GetMapping("/list")
+    public ResponseEntity<?> listController(){
+        List<String> stringList = new ArrayList<>();
+        stringList.add("깃 액션");
+        stringList.add("AWS EC2");
+        stringList.add("CI/CD");
+        return ResponseEntity.ok().body(stringList);
     }
 }
